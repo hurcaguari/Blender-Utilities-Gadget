2f77780ae6cc9186bfa5a9a37173548274bc25ce
hurcaguari <hurcaguari@outlook.com>
2025-01-30 22:59:50 +0800

    cmd

--- zip.cmd
+++ zip.cmd
@@ -2,10 +2,10 @@
 REM filepath: /c:/Users/Hurca/OneDrive/GitCode/Blender-Utilities-Gadget/zip.cmd
 
 REM 定义要压缩的文件夹和输出压缩包的路径
-set SOURCE_FOLDER=C:\Users\Hurca\OneDrive\GitCode\Blender-Utilities-Gadget
+set SOURCE_FOLDER=C:\Users\Hurca\OneDrive\GitCode\Blender-Utilities-Gadget/source
 set OUTPUT_ZIP=C:\Users\Hurca\OneDrive\GitCode\Blender-Utilities-Gadget.zip
 
 REM 使用 PowerShell 创建压缩包
-powershell -command "Compress-Archive -Path '%SOURCE_FOLDER%' -DestinationPath '%OUTPUT_ZIP%' -Force"
+powershell -command "Get-ChildItem -Path '%SOURCE_FOLDER%' | Compress-Archive -DestinationPath '%OUTPUT_ZIP%' -Force"
 
 echo 压缩完成: %OUTPUT_ZIP%
